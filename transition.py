5411fdd5081c3d42c92dd33fc3aa6eeb10556e91
Eli <eltrhn@gmail.com>
2018-03-06 22:57:22 -0800

    Start on parser. Feels badly-written

--- transition.py
+++ transition.py
@@ -61,30 +61,3 @@ class Transition(TransitionBase):
                 built.append(f'{v}{suffix}')
         return built
 
-def conv_permute(tr, total):
-    """
-    Given a new-style permutationally-symmetric transition:
-        total=8 (Moore neighborhood)
-        -------
-        1,0
-        1:4,0:4
-        1:4,0
-        1:3,1,0,0
-    Return its old-style representation:
-        1,1,1,1,0,0,0,0
-    Order is not preserved.
-    """
-    if isinstance(tr, str):
-        tr = tr.split(',')
-    # Balance unspecified values
-    seq = [i.partition(':')[::2] for i in map(str.strip, tr)]
-    # How many cells filled
-    tally = total - sum(int(i) for _, i in seq if i)
-    # And how many empty slots left to fill
-    empties = sum(1 for _, i in seq if not i)
-    # filler algo courtesy of Thomas Russell on math.stackexchange
-    # https://math.stackexchange.com/a/1081084
-    filler = (ceil((tally-k+1)/empties) for k in range(1, 1+empties))
-    gen = ((st, num if num else str(next(filler))) for st, num in seq)
-    return ','.join(utils.AdditiveDict(gen).expand())
-
--- utils.py
+++ utils.py
@@ -8,14 +8,36 @@ class AdditiveDict(dict):
     def expand(self):
         return chain(*(k*v for k, v in self.items()))
 
-class AutoAppendDict(dict):
-    def __init__(self, it):
-        for key, val in it:
-            self[key] = val
-    
-    def __setitem__(self, key, val):
-        super().__setitem__(key, self[key] + [val])
-    
-    def __missing__(self, key=None):
-        return []
+class Variable:
+    __slots__ = 'name', 'reps'
+    def __init__(self, name):
+        self.name = name
+        self.reps = 0
+
+def conv_permute(tr, total):
+    """
+    Given a new-style permutationally-symmetric transition:
+        total=8 (Moore neighborhood)
+        -------
+        1,0
+        1:4,0:4
+        1:4,0
+        1:3,1,0,0
+    Return its old-style representation:
+        1,1,1,1,0,0,0,0
+    Order is not preserved.
+    """
+    if isinstance(tr, str):
+        tr = tr.split(',')
+    # Balance unspecified values
+    seq = [i.partition(':')[::2] for i in map(str.strip, tr)]
+    # How many cells filled
+    tally = total - sum(int(i) for _, i in seq if i)
+    # And how many empty slots left to fill
+    empties = sum(1 for _, i in seq if not i)
+    # filler algo courtesy of Thomas Russell on math.stackexchange
+    # https://math.stackexchange.com/a/1081084
+    filler = (ceil((tally-k+1)/empties) for k in range(1, 1+empties))
+    gen = ((st, num if num else str(next(filler))) for st, num in seq)
+    return ','.join(utils.AdditiveDict(gen).expand())
 
