86c4c3c2af96919010d06d43f573749ce61e72ae
Eli <eltrhn@gmail.com>
2018-04-17 23:22:17 -0700

    Fix __all__ declaration redundancy
    (It was rewriting the whole value instead of just doing 'var __all__n = __all__0')

--- magic/compiler.py
+++ magic/compiler.py
@@ -17,9 +17,9 @@ def _handle_table(comp, tbl):
     for directive, value in tbl.directives.items():
         comp.append(f'{directive}: {value}')
     comp.append('')
-
+    
     for suf in range(1+tbl.var_all_rep):
-        comp.append(f'var __all__{suf} = {set(tbl.var_all)}')
+        comp.append(f"var __all__{suf} = {'__all__0' if suf else set(tbl.var_all)}")
     for var, value in tbl.vars.items():
         comp.append('')
         value = set(value)  # Removes duplicates and also gives it braces
