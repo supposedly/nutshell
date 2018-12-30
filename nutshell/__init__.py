c310e1cd006ddff1e0397c6b6e7c3ae7a5d3a70d
Eli <eltrhn@gmail.com>
2018-12-30 01:23:03 -0800

    Get rid of files related to old symmetries

--- nutshell/__init__.py
+++ nutshell/__init__.py
@@ -1,4 +1,3 @@
 __version__ = '0.6.0a'
 from .common import *
-from .segment_types.table import _napkins as napkin
-from .segment_types.table._napkins import Napkin, OrthNapkin, HexNapkin
+from . import segment_types
