0766de0b60e7e2ac94b435e920bcb41748cf0665
h <hi@how.hadi.is>
2020-11-07 01:36:27 -0800

    Update version number and examples

--- nutshell/__init__.py
+++ nutshell/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 from .common import *
 from .segment_types.table import _napkins as napkin
 from .segment_types.table._napkins import Napkin, OrthNapkin, HexNapkin
